b86bfa017616795ae256cd811dd0272f5ad9262f
Will Lewis <lumitedubbz@gmail.com>
2020-02-23 00:44:02 +0000

    Some work on conversion stuff. Temperatures basically done but I might change it tomorrow to use enums

--- GCSE/Unit_Converter/Program.cs
+++ GCSE/Unit_Converter/Program.cs
@@ -8,20 +8,49 @@ namespace Unit_Converter
     {
         public static void Main()
         {
+            Console.Write("Please enter a temperature: ");
+            
+            var startingMeasurement = double.Parse(Console.ReadLine());
 
+            Console.Write("Is this measurement in kelvin, fahrenheit or celsius? ");
+
+            var startingUnit = Console.ReadLine();
+
+            Console.Write("Is your desired unit kelvin, fahrenheit or celsius? ");
+
+            var desiredUnit = Console.ReadLine();
+
+            Console.WriteLine($"{startingMeasurement}{startingUnit[0].ToString().ToUpper()} is {startingMeasurement.ConvertTo(desiredUnit, startingUnit)}{desiredUnit[0].ToString().ToUpper()}.");
         }
     }
 
-    public sealed class ConversionType
+    public static class TemperatureConversions
     {
-        public string Name { get; set; }
-        public string Group { get; set; }
-    }
-
-    public sealed class Sum
-    {
-        public string Product { get; set; }
-        public int Formula { get; set; }
+        public static double ConvertTo(this double startingMeasurement, string desiredUnit, string startingUnit)
+        {
+            return startingUnit switch
+            {
+                "celsius" => (desiredUnit switch
+                {
+                    "kelvin" => (startingMeasurement + 273.15),
+                    "fahrenheit" => ((startingMeasurement * 1.8) + 32),
+                    _ => startingMeasurement
+                }),
+                "fahrenheit" => (desiredUnit switch
+                {
+                    "kelvin" => ((startingMeasurement + 459.67) * (5d / 9d)),
+                    "celsius" => ((startingMeasurement - 32) / 1.8),
+                    _ => startingMeasurement
+                }),
+                "kelvin" => (desiredUnit switch
+                {
+                    "celsius" => (startingMeasurement - 273.15),
+                    "fahrenheit" => (startingMeasurement * (9d / 5d) - 459.67),
+                    _ => startingMeasurement
+                }),
+                _ => startingMeasurement
+            };
+        }
     }
 }
 
